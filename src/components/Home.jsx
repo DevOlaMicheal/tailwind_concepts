779e26ab38e89d9e6898075323e17cfc9cf76a67
Micheal Ojedeji <97312093+DevOlaMicheal@users.noreply.github.com>
2023-02-22 12:35:41 +0100

    this error must disappear insha allah

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,26 +8,42 @@ export default function Home() {
 const [Measurements, setMeasurements] = useState([
     {
         title: "Ankara",
-        details: " ",
+        details: {
+            name: "John Doe",
+            date: "1/2/2023",
+            gender: "male"
+        },
         id: '0'
 
     },
     {
         title: "Senator",
-        details: "",
+        details: {
+            name: "mirabel michael",
+            date: "1/2/2023",
+            gender: "female"
+        },
         id: '1'
 
         
     },
     {
         title: "Corporate",
-        details: "",
+        details: {
+            name: "Oluwaseun Gladys",
+            date: "1/2/2023",
+            gender: "female"
+        },
         id: '3'
         
     },
     {
         title: "shirt and trouser",
-        details: "",
+        details: {
+            name: "mirabel michael",
+            date: "1/2/2023",
+            gender: "male"
+        },
         id: '4'
 
         
@@ -35,10 +51,19 @@ const [Measurements, setMeasurements] = useState([
 
 
 ])
+
+const handleDelete = (id) => {
+    const U = Measurements.filter(measures => measures.id !== id);
+
+    // setMeasurements(U)
+}
   return (
     <div className='container mx-auto py-3'>
         <h1 className='text-2xl text-center'>Home Page</h1>
-            <MeasureList M={Measurements}/>
+            <MeasureList M={Measurements} handleDelete={handleDelete} title="All Measures" />
+            {/* <MeasureList M={Measurements.filter((m) => m.details.gender === "male")} title="male Measures" /> */}
+          
+
     </div>
   )
 }
--- src/components/MeasurementList.jsx
+++ src/components/MeasurementList.jsx
@@ -1,37 +1,47 @@
 import React from "react";
 import pimage from '../assets/pro.png'
-export default function MeasureList({ M }) {
+export default function MeasureList({ M, title, handleDelete }) {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-3 gap-4 p-3'>
+    <div>
+      <h2 className="text-center pt-2">
+        { title }
+      </h2>
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-4 p-3'>
 
       {M.map((measure) => (
         <div
           className="bg-white text-center p-3 flex flex-col rounded-lg"
           key={measure.id}
         >
-          <div className="flex flex-row p-3">
-            <img src={pimage} alt="" className="w-1/3" />
-            <div className="p-2">
-              <p>Name: John Doe</p>
-              <p>Date: 12/2/2004 </p>
-              <p>gender: make</p>
+        
+            <div className="flex flex-col p-2">
+              <p>Name: {measure.details.name}</p>
+              <p>Date: {measure.details.date} </p>
+              <p>gender: {measure.details.gender}</p>
+              <p>status: </p>
             </div>
-          </div>
+          
          
           <h2 className="font-semibold text-2xl py-1 border-b-2 border-whitee text-center">{measure.title}</h2>
           
-          <p className="py-1">{measure.details}</p>
-
-          <div className="flex flex-col py-2">
-            <a
-              href="#"
-              className="border-[2px] text-center border-altlearn px-4 text-lg font-semibold rounded py-[8px] hover:bg-altlearn hover:text-white"
+          
+          <div className="flex flex-row justify-center py-2">
+          <button
+              className="border-[1px] text-center border-altlearn px-4 font-semibold rounded py-[8px] hover:bg-altlearn hover:text-white md:px-[6px] mr-1"
             >
-              show Details!
-            </a>
+              view details
+            </button>   
+                     <button
+              className="border-[1px] text-center border-green-500 px-4 font-semibold rounded py-[8px] hover:bg-green-500 hover:text-white  md:px-[6px]"
+
+              onClick={handleDelete(measure.id)}
+            >
+              Completed!
+            </button>
           </div>
         </div>
       ))}
+    </div>
     </div>
   );
 }
