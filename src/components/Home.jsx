413128ac9b35532a6476ab84bf013d125c7ef3cb
Micheal Ojedeji <97312093+DevOlaMicheal@users.noreply.github.com>
2023-02-23 17:31:01 +0100

    added json server, fetch api and handled errors

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,45 +7,42 @@ export default function Home() {
   const [Measurements, setMeasurements] = useState(null);
   const [Pending, setPending] = useState(true);
   const [error, setError] = useState(null);
- 
 
   useEffect(() => {
-    fetch('http://localhost:8000/Measurementss')
-      .then(res => {
-        if(!res.ok) {
-          throw Error("Could not fetch data, try again")
+    fetch("http://localhost:8000/Measurements")
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not fetch data, try again");
         }
-        return res.json()
-      }).then(data => {
-        setMeasurements(data);
-        setPending(false)
-      }).catch(err => {
-        setPending(false)
-        setError(err.message)
-  
+        return res.json();
       })
+      .then((data) => {
+        setMeasurements(data);
+        setPending(false);
+      })
+      .catch((err) => {
+        setPending(false);
+        setError(err.message);
+      });
   }, []);
 
   return (
-  
     <div className="container mx-auto py-3 top-0">
       <h1 className="text-2xl text-center">Home Page</h1>
-      {error &&
-      <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-4 text-center rounded relative my-[100px] m-2" role="alert">
-      <strong className="font-bold">opps! failed</strong> <br />
-      <span className="block sm:inline">{error}.</span>
-    
-    </div>
-       }
+      {error && (
+      < div
+          className="bg-red-100 border
+           border-red-400 text-red-700 
+          px-4 py-4 text-center 
+          rounded relative my-[100px] m-2"
+          role="alert"
+        >
+          <strong className="font-bold">opps! failed</strong> <br />
+          <span className="block sm:inline">{error}.</span>
+        </div>
+      )}
       {Pending && <div>Loading...</div>}
-      {Measurements &&  <MeasureList
-        M={Measurements}
-        title="All Measures"
-       
-      />
-
-      }
-      
+      {Measurements && <MeasureList M={Measurements} title="All Measures" />}
     </div>
   );
 }
