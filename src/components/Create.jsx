19bafa85de9e8e0b55019d893e85db28cd879c63
Micheal Ojedeji <97312093+DevOlaMicheal@users.noreply.github.com>
2023-03-06 09:35:44 +0100

    added create page and react router 5

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 export default function Create() {
   return (
-    <div className='py-3 container mx-auto'>
+    <div className='py-3 text-center container mx-auto md:text-start'>
       <h1 className="text-2xl">Add New Measurement</h1>
-    <span className='text-altlearn'>Fill all required fields marked (*)</span>      
+    <span className='text-altlearn my-2'>Fill all required fields marked (*)</span>  
+    <hr className="h-px my-8 bg-red-200 border-0 dark:bg-gray-700"></hr>    
         
     </div>
   )
